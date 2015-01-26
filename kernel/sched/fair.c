73aae64fda1972e67dde08f6bbe276686598eebe
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-01-26 19:47:28 +0000

    ANDROID: sched: Enable idle balance to pull single task towards cpu with higher capacity
    We do not want to miss out on the ability to pull a single remaining
    task from a potential source cpu towards an idle destination cpu. Add an
    extra criteria to need_active_balance() to kick off active load balance
    if the source cpu is over-utilized and has lower capacity than the
    destination cpu.
    
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Andres Oportus <andresoportus@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5788,6 +5788,11 @@ static unsigned long capacity_spare_wake(int cpu, struct task_struct *p)
 	return capacity_orig_of(cpu) - cpu_util_wake(cpu, p);
 }
 
+static bool cpu_overutilized(int cpu)
+{
+	return (capacity_of(cpu) * 1024) < (cpu_util(cpu) * capacity_margin);
+}
+
 /*
  * find_idlest_group finds and returns the least busy CPU group within the
  * domain.
@@ -8363,6 +8368,13 @@ static int need_active_balance(struct lb_env *env)
 	    (env->src_rq->cfs.h_nr_running == 1)) {
 		if ((check_cpu_capacity(env->src_rq, sd)) &&
 		    (capacity_of(env->src_cpu)*sd->imbalance_pct < capacity_of(env->dst_cpu)*100))
+			return 1;
+	}
+
+	if ((capacity_of(env->src_cpu) < capacity_of(env->dst_cpu)) &&
+				env->src_rq->cfs.h_nr_running == 1 &&
+				cpu_overutilized(env->src_cpu) &&
+				!cpu_overutilized(env->dst_cpu)) {
 			return 1;
 	}
 
